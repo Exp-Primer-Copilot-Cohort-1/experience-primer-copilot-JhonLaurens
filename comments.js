2a06440a84a2971cdc4d3d44f8ae3ae58007e9c8
JhonLaurens <jhonlaurens@gmail.com>
2023-11-07 01:27:50 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,20 +1,109 @@
-//Create a new server 
-const express = require('express');
-const router = express.Router();
-const commentsCtrl = require('../controllers/comments');
+// Create web server
+// Run: node comments.js
+// Open browser: http://localhost:3000
 
-router.post('/posts/:id/comments', isLoggedIn, commentsCtrl.create);
+// Import modules
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var qs = require('querystring');
 
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.redirect('/auth/google');
-  }
+// Create server
+http.createServer(function (req, res) {
+    // Get url
+    var pathname = url.parse(req.url).pathname;
+    // Get method
+    var method = req.method.toUpperCase();
 
-module.exports = router;
-
-
-
-
-
-
+    // Check url and method
+    if (pathname === '/' && method === 'GET') {
+        // Read file
+        fs.readFile('./comments.html', function (err, data) {
+            // Check read file
+            if (err) {
+                // Send response
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                });
+                res.end('404 Not Found');
+            } else {
+                // Send response
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                res.end(data);
+            }
+        });
+    } else if (pathname === '/comments' && method === 'GET') {
+        // Read file
+        fs.readFile('./comments.json', function (err, data) {
+            // Check read file
+            if (err) {
+                // Send response
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                });
+                res.end('404 Not Found');
+            } else {
+                // Send response
+                res.writeHead(200, {
+                    'Content-Type': 'application/json'
+                });
+                res.end(data);
+            }
+        });
+    } else if (pathname === '/comments' && method === 'POST') {
+        // Body
+        var body = '';
+        // Add listener for data
+        req.on('data', function (chunk) {
+            // Append chunk to body
+            body += chunk;
+        });
+        // Add listener for end
+        req.on('end', function () {
+            // Parse body
+            var params = qs.parse(body);
+            // Read file
+            fs.readFile('./comments.json', function (err, data) {
+                // Check read file
+                if (err) {
+                    // Send response
+                    res.writeHead(404, {
+                        'Content-Type': 'text/html'
+                    });
+                    res.end('404 Not Found');
+                } else {
+                    // Convert data
+                    var comments = JSON.parse(data);
+                    // Add comment
+                    comments.push(params);
+                    // Write file
+                    fs.writeFile('./comments.json', JSON.stringify(comments), function (err) {
+                        // Check write file
+                        if (err) {
+                            // Send response
+                            res.writeHead(500, {
+                                'Content-Type': 'text/html'
+                            });
+                            res.end('500 Server Error');
+                        } else {
+                            // Send response
+                            res.writeHead(200, {
+                                'Content-Type': 'application/json'
+                            });
+                            res.end(JSON.stringify(params));
+                        }
+                    });
+                }
+            });
+        });
+    } else {
+        // Send response
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
+        });
+        res.end('404 Not Found');
+    }
+}).listen(3000);
 
